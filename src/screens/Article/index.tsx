2a671ba13cf61a25e62a6008c0fdaaf195d6c349
charlescampista <charlescampista@gmail.com>
2022-03-26 23:22:55 -0300

    Adding Loading component to webvie

--- src/screens/Article/index.tsx
+++ src/screens/Article/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from "react-native";
 import WebView from "react-native-webview";
+import { Load } from "../../components/Load";
 import { styles } from "./styles";
 export function Article(props: any) {
   if (props.route.params.post) {
@@ -8,6 +9,9 @@ export function Article(props: any) {
         <WebView
           style={styles.webView}
           originWhitelist={["*"]}
+          renderLoading={() => {
+            return <Load />;
+          }}
           source={{ html: props.route.params.post.content }}
         />
       </View>
